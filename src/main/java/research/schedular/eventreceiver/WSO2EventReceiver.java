3d481b7b140c5655730fe97ae9270ab1fbc3b355
arosha <aroshar@hsenidmobile.com>
2017-11-10 00:23:45 +0530

    Added Edgar composite event creation.

--- src/main/java/research/schedular/eventreceiver/WSO2EventReceiver.java
+++ src/main/java/research/schedular/eventreceiver/WSO2EventReceiver.java
@@ -145,6 +145,12 @@ public class WSO2EventReceiver {
                         decodeCompositeEvent(event);
                     }
                 });
+            } else if(event.getStreamId().contains("outputHEEdgarStream")) {
+                decodingWorkers.submit(new Runnable() {
+                    public void run() {
+                        decodeCompositeEventEdgar(event);
+                    }
+                });
             } else {
                 count.incrementAndGet();
                 Object[] payloadData = event.getPayloadData();
@@ -214,6 +220,131 @@ public class WSO2EventReceiver {
                 String f3 = field3Builder.toString().replace("0", "");
                 String f4 = field4Builder.toString().replace("0", "");
                 if(f2.isEmpty() && (f3.isEmpty() || f4.isEmpty())) {
+//                            log.info("Filtered out event [" + decodedEvent + "]");
+                } else {
+                    decodedEvents.add(decodedEvent);
+                }
+            }
+            for(Event decodedEvent: decodedEvents) {
+                count.incrementAndGet();
+                double latency = (System.currentTimeMillis() - decodedEvent.getTimeStamp());
+                latencyValuesLock.lock();
+                heLatencyValuesLock.lock();
+                latencyValues.add(latency);
+                heLatencyValues.add(latency);
+                heLatencyValuesLock.unlock();
+                latencyValuesLock.unlock();
+            }
+        } catch (Throwable th) {
+            log.error("Error occurred while decoding [" + th + "], Event [" + event + "]");
+            th.printStackTrace();
+        }
+    }
+
+    private void decodeCompositeEventEdgar(Event event) {
+        try {
+            List<Event> decodedEvents = new ArrayList<Event>();
+            Object[] payloadData = event.getPayloadData();
+
+            String field1 = (String) payloadData[0];
+            Iterator<String> field1Arr = fieldSplitter.split(field1).iterator();
+
+            String field2 = (String) payloadData[1];
+            Iterator<String> field2Arr = fieldSplitter.split(field2).iterator();
+
+            String field4 = (String) payloadData[3];
+            Iterator<String> field4Arr = fieldSplitter.split(field4).iterator();
+
+            String field5 = (String) payloadData[4];
+            Iterator<String> field5Arr = fieldSplitter.split(field5).iterator();
+
+            String field6 = (String) payloadData[5];
+            Iterator<String> field6Arr = fieldSplitter.split(field6).iterator();
+
+            String field7 = (String) payloadData[6];
+            Iterator<String> field7Arr = fieldSplitter.split(field7).iterator();
+
+            String field10 = (String) payloadData[9];
+            Iterator<String> field10Arr = fieldSplitter.split(field10).iterator();
+
+            String field11 = (String) payloadData[10];
+            Iterator<String> field11Arr = fieldSplitter.split(field11).iterator();
+
+            String field12 = (String) payloadData[11];
+            Iterator<String> field12Arr = fieldSplitter.split(field12).iterator();
+
+            String field13 = (String) payloadData[12];
+            Iterator<String> field13Arr = fieldSplitter.split(field13).iterator();
+
+            String field14 = (String) payloadData[13];
+            Iterator<String> field14Arr = fieldSplitter.split(field14).iterator();
+
+            String field15 = (String) payloadData[14];
+            Iterator<String> field15Arr = fieldSplitter.split(field15).iterator();
+
+            String field16 = (String) payloadData[15];
+            Iterator<String> field16Arr = fieldSplitter.split(field16).iterator();
+
+            String field3 = (String) payloadData[2];
+            String decryptedField3 = homomorphicEncDecService.decryptLongVector(field3);
+            Iterator<String> decryptedField3Arr = commaSplitter.split(decryptedField3).iterator();
+
+            String field8 = (String) payloadData[7];
+            String decryptedField8 = homomorphicEncDecService.decryptLongVector(field8);
+            Iterator<String> decryptedField8Arr = commaSplitter.split(decryptedField8).iterator();
+
+            String field9 = (String) payloadData[8];
+            String decryptedField9 = homomorphicEncDecService.decryptLongVector(field9);
+            Iterator<String> decryptedField9Arr = commaSplitter.split(decryptedField9).iterator();
+
+
+            for(int i = 0; i < compositeEventSize; i++) {
+                StringBuilder field3Builder = new StringBuilder();
+                StringBuilder field8Builder = new StringBuilder();
+                StringBuilder field9Builder = new StringBuilder();
+                for(int j = 0; j < maxEmailLength; j++) {
+                    field3Builder.append(decryptedField3Arr.next());
+                    field8Builder.append(decryptedField8Arr.next());
+                    field9Builder.append(decryptedField9Arr.next());
+                }
+
+                String f2Val = (field2Arr.hasNext()) ? field2Arr.next() : "";
+                String f4Val = (field4Arr.hasNext()) ? field4Arr.next() : "";
+                String f5Val = (field5Arr.hasNext()) ? field5Arr.next() : "";
+                String f6Val = (field6Arr.hasNext()) ? field6Arr.next() : "";
+                String f7Val = (field7Arr.hasNext()) ? field7Arr.next() : "";
+                String f10Val = (field10Arr.hasNext()) ? field10Arr.next() : "";
+                String f11Val = (field11Arr.hasNext()) ? field11Arr.next() : "";
+                String f12Val = (field12Arr.hasNext()) ? field12Arr.next() : "";
+                String f13Val = (field13Arr.hasNext()) ? field13Arr.next() : "";
+                String f14Val = (field14Arr.hasNext()) ? field14Arr.next() : "";
+                String f15Val = (field15Arr.hasNext()) ? field15Arr.next() : "";
+                String f16Val = (field16Arr.hasNext()) ? field16Arr.next() : "";
+
+                String splitField1 = field1Arr.next();
+                Object[] payloadDataArray = {
+                        Long.valueOf(splitField1),
+                        f2Val,
+                        field3Builder.toString(),
+                        f4Val,
+                        f5Val,
+                        f6Val,
+                        f7Val,
+                        field8Builder.toString(),
+                        field9Builder.toString(),
+                        f10Val,
+                        f11Val,
+                        f12Val,
+                        f13Val,
+                        f14Val,
+                        f15Val,
+                        f16Val
+                };
+                Event decodedEvent = new Event(event.getStreamId(), Long.valueOf(splitField1), null, null, payloadDataArray);
+                String f3 = field3Builder.toString().replace("0", ""); // Try later: boolean isSatisfiedF2 = field2Builder.toString().equals("0000000000000000000000000000000000000000");
+                String f8 = field8Builder.toString().replace("0", "");
+                String f9 = field9Builder.toString().replace("0", "");
+                if(f3.isEmpty() && f8.isEmpty() && f9.isEmpty()) {
 //                            log.info("Filtered out event [" + decodedEvent + "]");
                 } else {
                     decodedEvents.add(decodedEvent);
